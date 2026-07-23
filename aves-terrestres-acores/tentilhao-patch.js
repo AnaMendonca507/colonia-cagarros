5835e1497f55fcb67015a5ee3d3761ac290acb4d
AnaMendonca507 <ana.mendonca6@gmail.com>
2026-07-23 16:05:57 +0000

    Adicionar vocalização de macho XC483594 ao tentilhão

--- aves-terrestres-acores/tentilhao-patch.js
+++ aves-terrestres-acores/tentilhao-patch.js
@@ -4,24 +4,35 @@
   const tentilhao = speciesLibrary.find((species) => species.id === 'tentilhao');
   if (!tentilhao) return;
 
-  const recording = {
-    id: '909663',
-    type: 'Vocalização de alarme',
-    sex: 'Macho',
-    location: 'Açores',
-    recordist: 'Consultar ficha do Xeno-canto'
-  };
+  const newRecordings = [
+    {
+      id: '909663',
+      type: 'Vocalização de alarme',
+      sex: 'Macho',
+      location: 'Açores',
+      recordist: 'Consultar ficha do Xeno-canto'
+    },
+    {
+      id: '483594',
+      type: 'Vocalização',
+      sex: 'Macho',
+      location: 'Açores',
+      recordist: 'Consultar ficha do Xeno-canto'
+    }
+  ];
 
-  tentilhao.recordings = [recording];
+  tentilhao.recordings = newRecordings;
 
   for (let index = recordings.length - 1; index >= 0; index -= 1) {
     if (recordings[index].speciesId === tentilhao.id) recordings.splice(index, 1);
   }
 
-  recordings.push({
-    ...recording,
-    speciesId: tentilhao.id,
-    speciesName: tentilhao.name
+  newRecordings.forEach((recording) => {
+    recordings.push({
+      ...recording,
+      speciesId: tentilhao.id,
+      speciesName: tentilhao.name
+    });
   });
 
   renderFilterChips(elements.sexFilters, uniqueSorted(recordings.map((item) => item.sex)), 'sex');
